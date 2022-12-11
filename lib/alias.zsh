28a67b2d494ab61b5e5f636261eeeae2453c3428
Mike <mike@slate.lan>
2022-12-11 13:08:31 -0500

    (chore) format

--- lib/alias.zsh
+++ lib/alias.zsh
@@ -3,29 +3,29 @@
 alias sudo='sudo '
 
 _exists() {
-  command -v $1 > /dev/null 2>&1
+	command -v $1 >/dev/null 2>&1
 }
 
 # Avoid stupidity with trash-cli:
 # https://github.com/sindresorhus/trash-cli
 # or use default rm -i
 if _exists trash; then
-  alias rm='trash'
+	alias rm='trash'
 fi
 
 # Just bcoz cls shorter than clear
 alias cls='clear'
 
 # Folders Shortcuts
-[ -d ~/Downloads ]                 && alias dl='cd ~/Downloads'
-[ -d ~/Desktop ]                   && alias dt='cd ~/Desktop'
-[ -d ~/Projects ]                  && alias pj='cd ~/Projects'
-[ -d ~/Projects/Notes ]            && alias pn='cd ~/Projects/Notes'
-[ -d ~/Projects/Code ]             && alias pc='cd ~/Projects/Code'
-[ -d ~/Projects/Code/Forks ]       && alias pcf='cd ~/Projects/Code/Forks'
-[ -d ~/Projects/Code/Work ]        && alias pcw='cd ~/Projects/Code/Work'
-[ -d ~/Projects/Code/Playground ]  && alias pcp='cd ~/Projects/Code/Playground'
-[ -d ~/Projects/Code/Repos ]       && alias pcr='cd ~/Projects/Code/Repos'
+# [ -d ~/Downloads ] && alias dl='cd ~/Downloads'
+# [ -d ~/Desktop ] && alias dt='cd ~/Desktop'
+# [ -d ~/code ] && alias pc='cd ~/code'
+# [ -d ~/Notes ] && alias pn='cd ~/Notes'
+# [ -d ~/Projects/Code ] && alias pc='cd ~/Projects/Code'
+# [ -d ~/Projects/Code/Forks ] && alias pcf='cd ~/Projects/Code/Forks'
+# [ -d ~/Projects/Code/Work ] && alias pcw='cd ~/Projects/Code/Work'
+# [ -d ~/Projects/Code/Playground ] && alias pcp='cd ~/Projects/Code/Playground'
+# [ -d ~/Projects/Code/Repos ] && alias pcr='cd ~/Projects/Code/Repos'
 
 # Commands Shortcuts
 alias e="$EDITOR"
@@ -61,44 +61,38 @@ alias get="curl -O -L"
 
 # Use tldr as help util
 if _exists tldr; then
-  alias help="tldr"
+	alias help="tldr"
 fi
 
 alias git-root='cd $(git rev-parse --show-toplevel)'
 
 if _exists lsd; then
-  unalias ls
-  alias ls='lsd'
-  alias lt='lsd --tree'
+	unalias ls
+	alias ls='lsd'
+	alias lt='lsd --tree'
 fi
 
 # cat with syntax highlighting
 # https://github.com/sharkdp/bat
 if _exists bat; then
-  alias cat='bat'
+	alias cat='bat'
 fi
 
-# Make ranger change to current directory on exit
 if _exists ranger; then
-  alias ranger='. ranger'
-fi
-
-# Byobu-tmux wrapper
-if _exists byobu && _exists tmux; then
-  alias tmux='byobu-tmux'
+	alias rd=". ranger"
 fi
 
 # Neovide and Lunarvim aliases
-if _exists neovide;then
+if _exists neovide; then
 
-  if ! _exists lvim; then
-    alias neovide='neovide --multigrid'
-  else
-    alias neovide='neovide --multigrid --neovim-bin="/usr/bin/lvim"'
-  fi
+	if ! _exists lvim; then
+		alias neovide='neovide'
+	else
+		alias neovide='neovide --neovim-bin="$HOME/.local/bin/lvim"'
+	fi
 
-  if _exists neovide-lunarvim; then
-    alias neovide='neovide-lunarvim --multigrid'
-  fi
+	if _exists neovide-lunarvim; then
+		alias neovide='neovide-lunarvim'
+	fi
 
 fi
