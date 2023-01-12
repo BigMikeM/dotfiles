d56789f42a3340898b143a85e6ed8f78f503fa3b
Mike Maurer <maumike87@gmail.com>
2023-01-11 23:01:46 -0500

    switch to blue for python formatter

--- home/config/lvim/config.lua
+++ home/config/lvim/config.lua
@@ -94,7 +94,7 @@ lvim.builtin.treesitter.highlight.enable = true
 -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { command = "black", filetypes = { "python" } },
+  { command = "blue", filetypes = { "python" } },
   { command = "isort", filetypes = { "python" } },
   {
     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
@@ -103,9 +103,8 @@ formatters.setup {
     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
     extra_args = { "--print-with", "100" },
     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "typescript", "typescriptreact" },
+    filetypes = { "typescript", "typescriptreact", "md", "markdown" },
   },
-  { command = "remark" }
 }
 
 -- set additional linters
@@ -468,6 +467,7 @@ lvim.autocommands = {
     },
   },
 }
+
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = { "*.json", "*.jsonc", "*.md" },
   command = "setlocal wrap",
