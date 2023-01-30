f149f59bb17eb41bf66d2eb6f911d15060bec729
Mike Maurer <maumike87@gmail.com>
2023-01-30 16:18:00 -0500

    add argdb alias

--- lib/alias.zsh
+++ lib/alias.zsh
@@ -97,6 +97,10 @@ if _exists neovide; then
 
 fi
 
+if _exists arm-none-eabi-gdb; then
+  alias agdb='arm-none-eabi-gdb'
+fi
+
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
