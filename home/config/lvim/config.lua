a335e4de3d3c8541725e9f8b1dd6f6c42bcc3a0d
Mike Maurer <maumike87@gmail.com>
2023-01-30 16:17:50 -0500

    update lunarvim config

--- home/config/lvim/config.lua
+++ home/config/lvim/config.lua
@@ -403,31 +403,31 @@ vim.keymap.set('n', ',W', swap_windows, { desc = 'Swap windows' })
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands = {
-  {
-    { "BufEnter", "Filetype" },
-    {
-      desc = "Open mini.map and exclude some filetypes",
-      pattern = { "*" },
-      callback = function()
-        local exclude_ft = {
-          "qf",
-          "NvimTree",
-          "toggleterm",
-          "TelescopePrompt",
-          "alpha",
-          "netrw",
-        }
+  -- {
+  --   { "BufEnter", "Filetype" },
+  --   {
+  --     desc = "Open mini.map and exclude some filetypes",
+  --     pattern = { "*" },
+  --     callback = function()
+  --       local exclude_ft = {
+  --         "qf",
+  --         "NvimTree",
+  --         "toggleterm",
+  --         "TelescopePrompt",
+  --         "alpha",
+  --         "netrw",
+  --       }
 
-        local map = require('mini.map')
-        if vim.tbl_contains(exclude_ft, vim.o.filetype) then
-          vim.b.minimap_disable = true
-          map.close()
-        elseif vim.o.buftype == "" then
-          map.open()
-        end
-      end,
-    },
-  },
+  --       local map = require('mini.map')
+  --       if vim.tbl_contains(exclude_ft, vim.o.filetype) then
+  --         vim.b.minimap_disable = true
+  --         map.close()
+  --       elseif vim.o.buftype == "" then
+  --         map.open()
+  --       end
+  --     end,
+  --   },
+  -- },
   {
     { "TextChangedI" },
     {
