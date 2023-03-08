59f87c2ea0761bab4915dba090becbaf4090a4f5
Mike Maurer <maumike87@gmail.com>
2023-03-07 20:33:13 -0500

    (lvim) update for kanagawa changes

--- home/config/lvim/config.lua
+++ home/config/lvim/config.lua
@@ -1,3 +1,40 @@
+require("kanagawa").setup({
+  compile = true,
+  colors = {
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = "none"
+        }
+      }
+    }
+  },
+  overrides = function(colors)
+    local theme = colors.theme
+    local palette = colors.palette
+    return {
+      TelescopeTitle = { fg = theme.ui.special, bold = true },
+      TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+      TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+      TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+      TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+      TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+      TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+      rainbowcol1 = { fg = palette.sakuraPink },
+      rainbowcol2 = { fg = palette.oniViolet },
+      rainbowcol3 = { fg = palette.surimiOrange },
+      rainbowcol4 = { fg = palette.lightBlue },
+      rainbowcol5 = { fg = palette.springViolet1 },
+      rainbowcol6 = { fg = palette.boatYellow1 },
+      rainbowcol7 = { fg = palette.peachRed },
+      Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+      PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+      PmenuSbar = { bg = theme.ui.bg_m1 },
+      PmenuThumb = { bg = theme.ui.bg_p2 },
+    }
+  end,
+}
+)
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
@@ -82,44 +119,9 @@ lvim.plugins = {
     cmd = "TroubleToggle",
   },
   { "tpope/vim-surround" },
-  { "rebelot/kanagawa.nvim",
-    config = function()
-      require("kanagawa").setup {
-        colors = {
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = "none"
-              }
-            }
-          }
-        },
-        overrides = function(colors)
-          local theme = colors.theme
-          local palette = colors.palette
-          return {
-            TelescopeTitle = { fg = theme.ui.special, bold = true },
-            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-            rainbowcol1 = { fg = palette.sakuraPink },
-            rainbowcol2 = { fg = palette.oniViolet },
-            rainbowcol3 = { fg = palette.surimiOrange },
-            rainbowcol4 = { fg = palette.lightBlue },
-            rainbowcol5 = { fg = palette.springViolet1 },
-            rainbowcol6 = { fg = palette.boatYellow1 },
-            rainbowcol7 = { fg = palette.peachRed },
-            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-            PmenuSbar = { bg = theme.ui.bg_m1 },
-            PmenuThumb = { bg = theme.ui.bg_p2 },
-          }
-        end,
-      }
-    end },
+  {
+    "rebelot/kanagawa.nvim",
+  },
   {
     "phaazon/hop.nvim",
     event = "BufRead",
