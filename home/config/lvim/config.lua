1b4f4d4f8f5c4e99126bb302eca65fe22466aa13
Mike Maurer <maumike87@gmail.com>
2023-04-04 20:55:17 -0400

    (lvim) update config

--- home/config/lvim/config.lua
+++ home/config/lvim/config.lua
@@ -69,6 +69,10 @@ formatters.setup {
 -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
+  {
+    command = "shellcheck",
+    filetypes = { "shell", "bash", "zsh", "sh" },
+  },
   -- { command = "marksman" },
 }
 
