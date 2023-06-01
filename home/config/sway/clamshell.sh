19a66d240c51d68b64b11059ae179838aa237ad9
Mike Maurer <maumike87@gmail.com>
2023-06-01 19:57:04 -0400

    (sway) correct clamshell.sh

--- home/config/sway/clamshell.sh
+++ home/config/sway/clamshell.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-if grep -q open /proc/acpi/button/lid/LID/state; then
+if grep -q open /proc/acpi/button/lid/LID0/state; then
     swaymsg output 'e-DP1' enable
 else
     swaymsg output 'e-DP1' disable
