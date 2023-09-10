38dd59617aa42b7c0f2b4e3db2f268bc0c718ab2
Mike Maurer <maumike87@gmail.com>
2023-09-10 03:11:00 -0400

    (neovide) begin settings

--- home/config/lvim/config.lua
+++ home/config/lvim/config.lua
@@ -12,6 +12,24 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 
+if vim.g.neovide then
+	-- Put anything you want to happen only in Neovide here
+	vim.o.guifont = "MonoLisa Nerd Font:h10" -- text below applies for VimScript
+	vim.g.neovide_padding_top = 0
+	vim.g.neovide_padding_bottom = 0
+	vim.g.neovide_padding_right = 0
+	vim.g.neovide_padding_left = 0
+	vim.g.neovide_floating_blur_amount_x = 2.0
+	vim.g.neovide_floating_blur_amount_y = 2.0
+	vim.g.neovide_hide_mouse_when_typing = true
+	vim.g.neovide_refresh_rate = 75
+	vim.g.neovide_refresh_rate_idle = 30
+	vim.g.neovide_cursor_antialiasing = true
+	vim.g.neovide_cursor_animate_in_insert_mode = true
+	vim.g.neovide_cursor_animate_command_line = true
+	vim.g.neovide_cursor_vfx_mode = "pixiedust"
+end
+
 -- LSP Settings
 require("lang.formatters")
 require("lang.linters")
