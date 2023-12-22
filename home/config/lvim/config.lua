68abaf2efd7cc3f0361b6df1270a16ed44693cc9
Mike Maurer <maumike87@gmail.com>
2023-12-22 13:08:24 -0500

    (lvim) add noice, remove fidget and signature due to conflict and duplication

--- home/config/lvim/config.lua
+++ home/config/lvim/config.lua
@@ -55,12 +55,6 @@ lvim.plugins = {
 			})
 		end,
 	},
-	{
-		"j-hui/fidget.nvim",
-		config = function()
-			require("fidget").setup()
-		end,
-	},
 	"ChristianChiarulli/swenv.nvim",
 	"stevearc/dressing.nvim",
 	"mfussenegger/nvim-dap-python",
@@ -186,13 +180,6 @@ lvim.plugins = {
 		end,
 	},
 	{
-		"ray-x/lsp_signature.nvim",
-		event = "BufRead",
-		config = function()
-			require("lsp_signature").on_attach()
-		end,
-	},
-	{
 		"simrat39/symbols-outline.nvim",
 		config = function()
 			require("symbols-outline").setup()
@@ -267,6 +254,42 @@ lvim.plugins = {
 		event = "InsertEnter",
 	},
 	{ "lambdalisue/suda.vim" },
+	-- lazy.nvim
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+		config = function()
+			require("noice").setup({
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true,
+					},
+				},
+				-- you can enable a preset for easier configuration
+				presets = {
+					bottom_search = false, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = true, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
+		end,
+	},
 }
 
 vim.api.nvim_create_autocmd("BufEnter", {
