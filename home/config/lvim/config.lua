40433efa1b11404c731a06ab8f43ff8f85c4540a
Mike Maurer <maumike87@gmail.com>
2024-03-18 19:20:39 -0400

    (lvim) add neotest deps

--- home/config/lvim/config.lua
+++ home/config/lvim/config.lua
@@ -92,7 +92,15 @@ lvim.plugins = {
 	"ChristianChiarulli/swenv.nvim",
 	"stevearc/dressing.nvim",
 	"mfussenegger/nvim-dap-python",
-	"nvim-neotest/neotest",
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
 	"nvim-neotest/neotest-python",
 	{
 		"folke/trouble.nvim",
