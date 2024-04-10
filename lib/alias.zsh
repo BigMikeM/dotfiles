97d8a363d9d72f62175c628fc2bc375c018e71c2
Mike Maurer <maumike87@gmail.com>
2024-04-09 23:08:59 -0400

    (alias) add alias for argcomplete3 -> argcomplete

--- lib/alias.zsh
+++ lib/alias.zsh
@@ -112,6 +112,11 @@ if _exists lazygit; then
 	alias lg='lazygit'
 fi
 
+## Argcomplete has a different name on debian systems
+if _exists register-python-argcomplete3; then
+	alias register-python-argcomplete='register-python-argcomplete3'
+fi
+
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
